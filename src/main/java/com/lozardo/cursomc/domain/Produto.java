802fa6205e869eaf0f83fdd5cc4b80ffdebdb332
Lucas Lozardo <lucas_losardo@hotmail.com>
2023-03-05 16:05:20 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/lozardo/cursomc/domain/Produto.java
+++ src/main/java/com/lozardo/cursomc/domain/Produto.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +25,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference    ///OMITIR A LISTA DE CATEGORIAS PARA CADA PRODUTO, PARA PARAR A ASSOCIACAO CICLICA, LOOP INFINITO ENTRE CATEGORIAS E PRODUTOS.
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 			joinColumns = @JoinColumn(name = "produto_id"),
